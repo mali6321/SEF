d84d46294a8e07d91273641a2ea7caf0905f7adf
Hassan Ali <muhammadhassanali154@gmail.com>
2021-04-22 21:36:36 -0400

    Update classifier.cpp

--- classifier.cpp
+++ classifier.cpp
@@ -8,15 +8,22 @@ class Classifier
 	int data;
 	int train();
 	int predict();
+
 };
-
-
-
 
 int main() 
 {
 	printf("The first one is for the classifier class and anything related to it it's data memeber\n");
+	
+}
+
+void train()
+{
 	printf("The second one is a train function\n");
+	printf("The secondFeature is added\n");
+}
+void predict() 
+{
 	printf("The third one is a predict function\n");
 
 }
