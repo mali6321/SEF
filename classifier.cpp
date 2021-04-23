8c659b65b5f998f8dab89ff137bded3ceaa2b650
Hassan Ali <muhammadhassanali154@gmail.com>
2021-04-22 21:28:49 -0400

    Create classifier.cpp

---
+++ classifier.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void train();
+void predict();
+
+class Classifier
+{
+	int data;
+	int train();
+	int predict();
+};
+
+
+
+
+int main() 
+{
+	printf("The first one is for the classifier class and anything related to it it's data memeber\n");
+	printf("The second one is a train function\n");
+	printf("The third one is a predict function\n");
+
+}
